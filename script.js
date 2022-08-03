3b2960bb397800905283e49636a0e1246cf77f36
Pascal <pascalyuen@gmail.com>
2022-08-04 01:18:14 +0800

    Add a grid of divs

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+
+  for (let j = 0; j < 16; j++) {
+    const col = document.createElement('div');
+    col.classList.add('col');
+    row.appendChild(col);
+  }
+
+  container.appendChild(row);
+}

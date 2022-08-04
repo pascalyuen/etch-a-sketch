ec10b78d4f8a678a53314896d25806fa2b9212e3
Pascal <pascalyuen@gmail.com>
2022-08-04 22:40:22 +0800

    Allow user to choose new grid size

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('.button');
+let num = 0;
 
 // Create a 16x16 grid of divs
 for (let i = 0; i < 16; i++) {
@@ -14,18 +16,57 @@ for (let i = 0; i < 16; i++) {
   }
 
   container.appendChild(row);
+  addGridHovering();
 }
 
-// Add event listener for each of the grids
-const cols = document.querySelectorAll('.col');
-cols.forEach(col => col.addEventListener('mouseenter', enterEvent));
-// cols.forEach(col => col.addEventListener('mouseleave', leaveEvent));
+
+// Add event listener for button
+button.addEventListener('click', getNewGridSize);
+button.addEventListener('click', removeOldGrid);
+button.addEventListener('click', makeNewGrid);
+button.addEventListener('click', addGridHovering);
+
+// Functions
+function addGridHovering() {
+  const cols = document.querySelectorAll('.col');
+  cols.forEach(col => col.addEventListener('mouseenter', enterEvent));
+}
 
 
 function enterEvent(e) {
   e.target.classList.add('hovering');
 }
 
-// function leaveEvent(e) {
-//   e.target.classList.remove('hovering');
-// }
+
+function getNewGridSize(e) {
+  num = parseInt(prompt('New grid size: '));
+  if (!Number.isInteger(num)) {
+    alert('Enter an integer.');
+  }
+  num = (num >= 100) ? 100 : num;
+}
+
+
+function removeOldGrid(e) {
+  while(container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+
+function makeNewGrid(e) {
+  for (let i = 0; i < num; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    row.classList.add('grid');
+  
+    for (let j = 0; j < num; j++) {
+      const col = document.createElement('div');
+      col.classList.add('col');
+      col.classList.add('grid');
+      row.appendChild(col);
+    }
+  
+    container.appendChild(row);
+  }
+}
